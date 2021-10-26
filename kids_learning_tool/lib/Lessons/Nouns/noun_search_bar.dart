8da764b187352000129c7e862faa4e25ff79da74
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-10-27 01:27:36 +0600

    before trying search delegate library

--- kids_learning_tool/lib/Lessons/Nouns/noun_search_bar.dart
+++ kids_learning_tool/lib/Lessons/Nouns/noun_search_bar.dart
@@ -1,38 +1,40 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/name_list.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/names.dart';
 
-class SearchBar extends StatefulWidget with PreferredSizeWidget {
+class SearchNoun extends StatefulWidget {
+  //with PreferredSizeWidget
   @override
-  State<SearchBar> createState() => _SearchBarState();
+  State<SearchNoun> createState() => _SearchNounState();
 
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  //@override
+  //Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
 
-class _SearchBarState extends State<SearchBar> {
-  late Icon _searchIcon;
-  late Widget _title;
+class _SearchNounState extends State<SearchNoun> {
+  //late Icon _searchIcon;
+  //late Widget _title;
 
   final globalKey = GlobalKey<ScaffoldState>();
   final TextEditingController _controller = TextEditingController();
-
+  final dio = Dio();
   NameList nameList = NameList();
   late List<Name> _list;
-  late bool _isSearching;
+  //late bool _isSearching;
   String _searchText = "";
-  List searchresult = [];
+  late List<Name> _searchresult;
+  List<Name> _showNames = [];
 
-  _SearchBarState() {
+  _SearchNounState() {
     _controller.addListener(() {
       if (_controller.text.isEmpty) {
         setState(() {
-          _isSearching = false;
           _searchText = "";
+          _list = _showNames;
         });
       } else {
         setState(() {
-          _isSearching = true;
           _searchText = _controller.text;
         });
       }
@@ -41,77 +43,124 @@ class _SearchBarState extends State<SearchBar> {
 
   @override
   void initState() {
-    _searchIcon = const Icon(Icons.search_sharp);
-    _title = const Text('Noun');
+    //_searchIcon = const Icon(Icons.search_sharp);
+    //_title = const Text('Noun');
     _list = nameList.getList();
-    _isSearching = false;
+    // _isSearching = false;
+    _searchOperation();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: _title,
-      centerTitle: true,
-      automaticallyImplyLeading: true,
-      actions: [
-        IconButton(
-            onPressed: () {
-              //print('called');
-              setState(() {
-                if (_searchIcon.icon == Icons.search_sharp) {
-                  // print('if');
-                  _searchIcon = const Icon(Icons.cancel);
-                  _title = const ListTile(
-                      leading: Icon(
-                        Icons.search_sharp,
-                        color: Colors.white10,
-                        size: 28,
-                      ),
-                      title: TextField(
-                        cursorColor: Colors.black,
-                        decoration: InputDecoration(
-                          hintText: 'Search a noun..',
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                            fontStyle: FontStyle.italic,
-                          ),
-                          border: InputBorder.none,
-                        ),
-                        style: TextStyle(color: Colors.white),
-                        //onChanged: searchOperation(_searchText),
-                      ));
-                  setState(() {
-                    _isSearching = true;
-                  });
-                } else {
-                  _searchIcon = const Icon(Icons.search_sharp);
-                  _title = const Text('Noun');
-                }
-              });
-            },
-            icon: _searchIcon)
-      ],
+    return Scaffold(
+      appBar: _myAppBar(context),
+      body: Container(
+        child: _buildList(),
+      ),
+      resizeToAvoidBottomInset: false,
     );
   }
 
-  void searchOperation(String searchText) {
-    searchresult.clear();
-    if (_isSearching != null) {
-      for (int i = 0; i < _list.length; i++) {
-        String textData = _list[i].text;
-        if (textData.toLowerCase().contains(searchText.toLowerCase())) {
-          searchresult.add(textData);
+  Widget _buildList() {
+    if (!(_searchText.isEmpty)) {
+      late List tempList; // = new List();
+      for (int i = 0; i < _showNames.length; i++) {
+        if (_showNames[i]
+            .text
+            .toLowerCase()
+            .contains(_searchText.toLowerCase())) {
+          _searchresult.add(_showNames[i]);
         }
       }
+      _list = _searchresult;
     }
+    return ListView.builder(
+      itemCount: _showNames == null ? 0 : _list.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: Text(_list[index].text),
+          onTap: () => print(_list[index].text),
+        );
+      },
+    );
+  }
+
+  AppBar _myAppBar(BuildContext context) {
+    return AppBar(
+      title: const ListTile(
+          leading: Icon(
+            Icons.search_sharp,
+            color: Colors.white10,
+            size: 28,
+          ),
+          title: TextField(
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              hintText: 'Search a noun..',
+              hintStyle: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+              ),
+              border: InputBorder.none,
+            ),
+            style: TextStyle(color: Colors.white),
+            //onChanged: searchOperation(_searchText),
+          )),
+      centerTitle: true,
+    );
+  }
+
+  void _searchOperation() async {
+    final response = await dio.get('https://swapi.co/api/people');
+    print(response);
+    List _tempList = []; // = new List();
+    for (int i = 0; i < response.data['results'].length; i++) {
+      _tempList.add(response.data['results'][i]);
+    }
+    setState(() {
+      _showNames = _searchresult;
+      _showNames.shuffle();
+      _list = _showNames;
+    });
   }
 }
 
-// class MyAppBar extends AppBar {
-//   const MyAppBar({key key, Widget title})
-//       : super(
-//           key: key,
-//           title: title,
-//         );
+// /_title = const ListTile(
+
+// void searchOperation(String searchText) {
+//   searchresult.clear();
+//   if (_isSearching != null) {
+//     for (int i = 0; i < _list.length; i++) {
+//       String textData = _list[i].text;
+//       if (textData.toLowerCase().contains(searchText.toLowerCase())) {
+//         searchresult.add(textData);
+//       }
+//     }
+//   }
 // }
+
+//AppBar:
+// title: _title,
+// centerTitle: true,
+// automaticallyImplyLeading: true,
+// actions: [
+//   IconButton(
+//       onPressed: () {
+//         //print('called');
+//         setState(() {
+//           if (_searchIcon.icon == Icons.search_sharp) {
+//             // print('if');
+//             _searchIcon = const Icon(Icons.cancel);
+
+// setState(() {
+//   _isSearching = true;
+// });
+// } else {
+//   _searchIcon = const Icon(Icons.search_sharp);
+//   _title = const Text('Noun');
+// }
+//         });
+//       },
+//       icon: _searchIcon)
+// ],
