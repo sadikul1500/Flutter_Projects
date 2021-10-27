c6386542045dbc7db483eb4d1bae44bd621e1a91
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-10-28 03:23:12 +0600

    slider added

--- kids_learning_tool/lib/Lessons/Nouns/noun_card.dart
+++ kids_learning_tool/lib/Lessons/Nouns/noun_card.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/names.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class NounCard extends StatelessWidget {
+class NounCard extends StatefulWidget {
   final Name name;
   NounCard({required this.name});
 
+  @override
+  State<NounCard> createState() => _NounCardState();
+}
+
+class _NounCardState extends State<NounCard> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -13,7 +20,7 @@ class NounCard extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Text(
-              'Noun: ${name.text}',
+              'Noun: ${widget.name.text}',
               style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.w500,
@@ -21,10 +28,51 @@ class NounCard extends StatelessWidget {
             ),
             const SizedBox(height: 15.0),
             Text(
-              'Meaning: ${name.meaning}',
+              'Meaning: ${widget.name.meaning}',
               style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: 400,
+              width: 600,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 385.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: const Duration(milliseconds: 900),
+                  viewportFraction: 0.8,
+                ),
+                items: widget.name.imgList.map((item) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Container(
+                      height: 250,
+                      margin: const EdgeInsets.symmetric(vertical: 0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.blueGrey,
+                            blurRadius: 3,
+                            spreadRadius: 3,
+                          ),
+                        ],
+                      ),
+                      child: Image.asset(
+                        item,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  );
+                }).toList(),
               ),
             ),
           ],
