395e7321069ff9b8a512c703e7bccd1c576572a5
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-10-28 03:20:26 +0600

    image added with text

--- kids_learning_tool/lib/Lessons/Nouns/names.dart
+++ kids_learning_tool/lib/Lessons/Nouns/names.dart
@@ -1,6 +1,28 @@
+import 'dart:async';
+import 'dart:io';
+
 class Name {
   String text;
   String meaning;
+  String dir;
+  List<String> imgList = [];
 
-  Name(this.text, this.meaning);
+  Name(this.text, this.meaning, this.dir) {
+    listDir(dir);
+  }
+
+  Future listDir(String folderPath) async {
+    var directory = Directory(folderPath);
+    //print(directory);
+
+    var exists = await directory.exists();
+    if (exists) {
+      directory
+          .list(recursive: true, followLinks: false)
+          .listen((FileSystemEntity entity) {
+        String path = entity.path.replaceAll('\\', '/');
+        imgList.add(path);
+      });
+    }
+  }
 }
