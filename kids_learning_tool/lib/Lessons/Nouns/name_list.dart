e2ac0528422aac8823929c279b9e0467ce7370c7
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-10-28 03:18:37 +0600

    image slider

--- kids_learning_tool/lib/Lessons/Nouns/name_list.dart
+++ kids_learning_tool/lib/Lessons/Nouns/name_list.dart
@@ -1,19 +1,22 @@
+//import 'dart:convert';
+
 import 'package:kids_learning_tool/Lessons/Nouns/names.dart';
+//import 'dart:io';
 
 class NameList {
   late List<Name> names;
 
-  nameList() {
+  NameList() {
     names = [
-      Name('Umbrella', 'ছাতা'),
-      Name('Apple', 'আপেল'),
-      Name('Car', 'গাড়ি'),
-      Name('Deer', 'হরিণ'),
+      Name('Umbrella', 'ছাতা', 'assets/nouns/Umbrella'),
+      Name('Apple', 'আপেল', 'assets/nouns/Apple'),
+      Name('Car', 'গাড়ি', 'assets/nouns/Car'),
+      Name('Deer', 'হরিণ', 'assets/nouns/Deer'),
     ];
   }
 
-  void add(String text, String meaning) {
-    names.add(Name(text, meaning));
+  void add(String text, String meaning, String dir) {
+    names.add(Name(text, meaning, dir));
   }
 
   List<Name> getList() {
