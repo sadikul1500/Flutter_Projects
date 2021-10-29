48a2902680a15f239ed598af718e60215443ddc0
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-10-29 10:56:00 +0600

    after solving async

--- kids_learning_tool/lib/main.dart
+++ kids_learning_tool/lib/main.dart
@@ -2,21 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:kids_learning_tool/Home/home.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/noun.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/noun_form.dart';
-//import 'package:kids_learning_tool/Lessons/Nouns/noun_search_bar.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: '/home',
     routes: {
-      // '/': (context) => Loading(),
       '/home': (context) => Home(),
       '/noun': (context) => Noun(),
       '/nounForm': (context) => NounForm(),
-<<<<<<< HEAD
-=======
-      /
->>>>>>> 26fe921e79c117784b86eb4f49ca1d8111aa910f
     },
   ));
 }
