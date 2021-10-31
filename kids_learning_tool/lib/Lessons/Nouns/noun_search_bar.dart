af1fb5126722b0cc4637ccbb8ffa22c501fa3384
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-10-31 18:36:16 +0600

    search shows list

--- kids_learning_tool/lib/Lessons/Nouns/noun_search_bar.dart
+++ kids_learning_tool/lib/Lessons/Nouns/noun_search_bar.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/name_list.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/names.dart';
+//import 'package:kids_learning_tool/Lessons/Nouns/noun.dart';
 
 class CustomDelegate extends SearchDelegate<String> {
   NameList nameList = NameList();
-  List<String> data = [];
+  // List<String> data = [];
   List<Name> names = [];
+  //Noun nouns = Noun();
 
-  CustomDelegate() {
-    names = nameList.getList();
-    for (final e in names) {
-      data.add(e.text);
-    }
+  CustomDelegate(this.names) {
+    //loading();
+    // loadData().then((data) {
+    //names = data;
+    // });
+    //names = nouns.names
+    // print(names.length);
+    // for (Name name in names) {
+    //   data.add(name.text);
+    // }
+    // print(data);
     //print('reached');
+  }
+
+  Future loadData() async {
+    names = nameList.getList();
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    if (names.isEmpty) {
+      return [];
+    }
+    // print(1223);
+    // print(names);
   }
 
   @override
@@ -29,26 +48,52 @@ class CustomDelegate extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    List<String> listToShow;
-    if (query.isNotEmpty) {
-      listToShow = data
-          .where((e) =>
-              e.toLowerCase().contains(query.toLowerCase()) &&
-              e.toLowerCase().startsWith(query.toLowerCase()))
-          .toList();
-    } else {
-      listToShow = data;
-    }
+    // print('building.......');
+    // List<String> listToShow;
+    // if (query.isNotEmpty) {
+    //   listToShow = data
+    //       .where((e) =>
+    //           e.toLowerCase().contains(query.toLowerCase()) &&
+    //           e.toLowerCase().startsWith(query.toLowerCase()))
+    //       .toList();
+    // } else {
+    //   listToShow = data;
+    // }
 
     return ListView.builder(
-      itemCount: listToShow.length,
+      itemCount: names.length,
       itemBuilder: (_, i) {
-        var noun = listToShow[i];
+        var noun = names[i].text;
+        // print('calling listview builder...');
         return ListTile(
           title: Text(noun),
           onTap: () => close(context, noun),
         );
       },
     );
+
+    // return FutureBuilder(
+    //   future: loadData(),
+    //   builder: (context, AsyncSnapshot snapshot) {
+    //     print(snapshot.hasData);
+    //     if (!snapshot.hasData) {
+    //       return Center(child: CircularProgressIndicator());
+    //     } else {
+    //       return Container(
+    //           child: ListView.builder(
+    //         itemCount: names.length,
+    //         scrollDirection: Axis.horizontal,
+    //         itemBuilder: (_, i) {
+    //           var noun = names[i].text;
+    //           print('calling listview builder...');
+    //           return ListTile(
+    //             title: Text(noun),
+    //             onTap: () => close(context, noun),
+    //           );
+    //         },
+    //       ));
+    //     }
+    //   },
+    // );
   }
 }
