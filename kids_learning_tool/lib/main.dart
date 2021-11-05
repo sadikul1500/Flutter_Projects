a01b0be7e071d2de3a9dfd542cdeaa29b76f6d7f
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-11-05 17:07:46 +0600

    audio added

--- kids_learning_tool/lib/main.dart
+++ kids_learning_tool/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:kids_learning_tool/Home/home.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/noun.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/noun_form.dart';
+import 'package:libwinmedia/libwinmedia.dart';
+//import 'package:kplayer/kplayer.dart';
 
 void main() {
+  LWM.initialize();
+  //AudioPlayer.setMockInitialValues({});
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: '/home',
