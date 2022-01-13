b040baac410644bd8e48566046df8e32fea89059
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2022-01-13 14:18:10 +0600

    new route added- quiz, matching

--- kids_learning_tool/lib/main.dart
+++ kids_learning_tool/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
+//import 'package:just_audio/just_audio.dart';
 import 'package:kids_learning_tool/Home/home.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/noun.dart';
 import 'package:kids_learning_tool/Lessons/Nouns/noun_form.dart';
+import 'package:kids_learning_tool/Quiz/quiz.dart';
 import 'package:libwinmedia/libwinmedia.dart';
+
+import 'Quiz/Matching/matching.dart';
 //import 'package:kplayer/kplayer.dart';
 
 void main() {
@@ -16,6 +19,8 @@ void main() {
       '/home': (context) => Home(),
       '/noun': (context) => Noun(),
       '/nounForm': (context) => NounForm(),
+      '/quiz': (context) => Quiz(),
+      '/matching': (context) => Matching(),
     },
   ));
 }
