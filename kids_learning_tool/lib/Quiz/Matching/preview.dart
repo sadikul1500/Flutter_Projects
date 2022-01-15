4e19fb4c372c98ab1239460d4550a404e1e5ea93
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2022-01-15 10:33:07 +0600

    preccessing. not tested

--- kids_learning_tool/lib/Quiz/Matching/preview.dart
+++ kids_learning_tool/lib/Quiz/Matching/preview.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:kids_learning_tool/Quiz/Matching/question.dart';
 
 class Preview extends StatefulWidget {
   final Question question;
-  Preview(this.question);
+  const Preview(this.question);
 
   @override
   State<Preview> createState() => PreviewState();
@@ -19,21 +22,33 @@ class PreviewState extends State<Preview> {
         title: const Text('Quiz Test: Matching'),
         centerTitle: true,
       ),
-      body: const MyStatefulWidget(),
+      body: MyStatefulWidget(widget.question),
     );
   }
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
+  //const MyStatefulWidget({Key? key}) : super(key: key);
+  final Question question;
+  const MyStatefulWidget(this.question);
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
 /// This is the private State class that goes with MyStatefulWidget.
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late Timer _timer;
+  int _start = 0;
+  bool hasPressed = false;
+  bool isCorrect = false;
+
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _start++;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,10 +66,106 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             alignment: const Alignment(0, 0),
             child: Column(
               children: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      startTimer();
+                    },
+                    child: const Text('Start')),
                 Container(
                   decoration: const BoxDecoration(
                       color: Colors.blueAccent, shape: BoxShape.circle),
-                )
+                  child: Text('$_start'),
+                ),
+                SizedBox(
+                    height: 300,
+                    width: 400,
+                    child: Image.file(
+                      File(widget.question.imagePath),
+                      fit: BoxFit.fill,
+                      filterQuality: FilterQuality.high,
+                    )),
+                const SizedBox(height: 10),
+                Text(widget.question.ques),
+                const SizedBox(height: 10),
+                Row(
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        if (widget.question.correctAnswer == 'A') {
+                          setState(() {
+                            isCorrect = true;
+                          });
+                        }
+                      },
+                      child: Text('A. ' + widget.question.options[0]),
+                      style: ElevatedButton.styleFrom(
+                          primary: hasPressed
+                              ? (isCorrect ? Colors.green : Colors.red)
+                              : Colors.blueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          )),
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (widget.question.correctAnswer == 'B') {
+                          setState(() {
+                            isCorrect = true;
+                          });
+                        }
+                      },
+                      child: Text('B. ' + widget.question.options[1]),
+                      style: ElevatedButton.styleFrom(
+                          primary: hasPressed
+                              ? (isCorrect ? Colors.green : Colors.red)
+                              : Colors.blueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          )),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        if (widget.question.correctAnswer == 'C') {
+                          setState(() {
+                            isCorrect = true;
+                          });
+                        }
+                      },
+                      child: Text('C. ' + widget.question.options[2]),
+                      style: ElevatedButton.styleFrom(
+                          primary: hasPressed
+                              ? (isCorrect ? Colors.green : Colors.red)
+                              : Colors.blueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          )),
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (widget.question.correctAnswer == 'D') {
+                          setState(() {
+                            isCorrect = true;
+                          });
+                        }
+                      },
+                      child: Text('D. ' + widget.question.options[3]),
+                      style: ElevatedButton.styleFrom(
+                          primary: hasPressed
+                              ? (isCorrect ? Colors.green : Colors.red)
+                              : Colors.blueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          )),
+                    ),
+                  ],
+                ),
               ],
             )),
       ),
