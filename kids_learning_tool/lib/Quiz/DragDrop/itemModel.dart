4d08269e6b53331badfacc17305a7b81e890932f
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2022-01-21 00:31:07 +0600

    Update itemModel.dart

--- kids_learning_tool/lib/Quiz/DragDrop/itemModel.dart
+++ kids_learning_tool/lib/Quiz/DragDrop/itemModel.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
 class ItemModel {
-  final String name;
-  final String value;
-  final IconData icon;
+  String name;
+  String value;
+  IconData icon;
   bool accepting;
 
   ItemModel(this.name, this.value, this.icon, {this.accepting = false});
